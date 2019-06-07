8e54f02c27a8c16d5e7efc0b6a7f23e150b0eb46
pabliyo <pablofeliz1989@gmail.com>
2019-06-07 02:00:13 -0300

    modificaciones en la vista, se movio el archivo main.gsp de la carpeta layouts, modicifacion de application.groovy y UrlMappings.groovy

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -9,9 +9,10 @@ grails.plugin.springsecurity.logout.postOnly = false
 
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/main',           access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
 	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index2.gsp',      access: ['permitAll']],
+	[pattern: '/index2.gsp',     access: ['permitAll']],
 	[pattern: '/shutdown',       access: ['permitAll']],
 	[pattern: '/assets/**',      access: ['permitAll']],
 	[pattern: '/**/js/**',       access: ['permitAll']],
